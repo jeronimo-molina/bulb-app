c263f1258774241c2880c7295527d248fc643586
jeronimo-molina <jeronimo@derossimolina.com.br>
2025-02-16 20:58:42 -0300

    "Material Design"

--- app.py
+++ app.py
@@ -1,22 +1,19 @@
 from flask import Flask, render_template, request, session, redirect, url_for
 from flask_babel import Babel, _
-from bulb_calc import calculate_wet_bulb, get_weather_data, get_alert_message
+from bulb_calc import calculate_wet_bulb, get_weather_data, get_forecast_data, get_alert_message
 
 app = Flask(__name__)
-app.config['BABEL_DEFAULT_LOCALE'] = 'en'  # Defina o idioma padrão
-app.config['BABEL_SUPPORTED_LOCALES'] = ['en', 'pt', 'es']  # Adicione os idiomas suportados
-app.secret_key = 'jrm210516###'  # Chave secreta para a sessão
+app.config['BABEL_DEFAULT_LOCALE'] = 'en'
+app.config['BABEL_SUPPORTED_LOCALES'] = ['en', 'pt', 'es']
+app.secret_key = 'your_secret_key'
 
 babel = Babel(app)
 
-# Callback para seleção de idioma
 def get_locale():
-    # Prioridade: Query String > Sessão > Padrão
-    return session.get('lang') or request.args.get('lang') or app.config['BABEL_DEFAULT_LOCALE']
+    return request.args.get('lang') or session.get('lang') or app.config['BABEL_DEFAULT_LOCALE']
 
 babel.init_app(app, locale_selector=get_locale)
 
-# Adicione get_locale ao contexto global
 @app.context_processor
 def inject_get_locale():
     return dict(get_locale=get_locale)
@@ -32,19 +29,18 @@ def index():
             country = weather_data['country']
             wet_bulb_temp = calculate_wet_bulb(temp, humidity)
             alert_message = get_alert_message(wet_bulb_temp)
-            return render_template("index.html", temperature=temp, humidity=humidity, wet_bulb_temperature=wet_bulb_temp, alert_message=alert_message, city=city, country=country)
+            forecast_data = get_forecast_data(city)
+            return render_template("index.html", temperature=temp, humidity=humidity, wet_bulb_temperature=wet_bulb_temp, alert_message=alert_message, city=city, country=country, forecast_data=forecast_data)
         except Exception as e:
             return render_template("index.html", error=str(e))
     return render_template("index.html")
 
-# Rota para alterar o idioma
 @app.route('/change-language')
 def change_language():
     lang = request.args.get('lang')
     if lang in app.config['BABEL_SUPPORTED_LOCALES']:
-        session['lang'] = lang  # Salva idioma na sessão
+        session['lang'] = lang
     return redirect(url_for('index'))
 
-
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)  # <-- Defina host e port aqui também
+    app.run(debug=True)
--- bulb_calc.py
+++ bulb_calc.py
@@ -1,6 +1,7 @@
 import os
 import requests
 import math
+import datetime
 
 def calculate_wet_bulb(temp, humidity):
     """
@@ -28,9 +29,33 @@ def get_weather_data(city):
         humidity = data['main']['humidity']
         country_code = data['sys']['country']
 
-        return {"temperature": temp, "humidity": humidity, "country": country_code}
+        # Obtenha a bandeira do país usando a API do countryflags.io
+        flag_url = f'https://countryflagsapi.com/png/{country_code}'
+
+        return {"temperature": temp, "humidity": humidity, "country": country_code, "flag": flag_url}
     except Exception as e:
         raise Exception(f"Erro ao buscar dados meteorológicos: {e}")
+
+def get_forecast_data(city):
+    """
+    Obtém dados meteorológicos futuros para a cidade usando a API do OpenWeatherMap.
+    """
+    try:
+        # Obtenha os dados meteorológicos futuros da cidade
+        request_url = f'http://api.openweathermap.org/data/2.5/forecast?appid={os.getenv("API_KEY")}&q={city}&units=metric'
+        data = requests.get(request_url).json()
+        forecast_data = []
+        for forecast in data['list']:
+            if "06:00:00" in forecast['dt_txt']:
+                forecast_data.append({
+                    "date": forecast['dt_txt'],
+                    "temperature": forecast['main']['temp'],
+                    "humidity": forecast['main']['humidity'],
+                    "condition": forecast['weather'][0]['description']
+                })
+        return forecast_data
+    except Exception as e:
+        raise Exception(f"Erro ao buscar dados de previsão meteorológica: {e}")
 
 def get_alert_message(wet_bulb_temp):
     """
@@ -43,7 +68,7 @@ def get_alert_message(wet_bulb_temp):
     elif wet_bulb_temp >= 23:
         return "Alert! Look for a cool place."
     else:
-        return "Normal temperature."
+        return "Normal bulb temperature."
 
 if __name__ == "__main__":
     city = input("Digite o nome da cidade: ")
@@ -55,14 +80,21 @@ if __name__ == "__main__":
             temp = weather_data['temperature']
             humidity = weather_data['humidity']
             country = weather_data['country']
-            flag = weather_data['flag']
             wet_bulb_temp = calculate_wet_bulb(temp, humidity)
             alert_message = get_alert_message(wet_bulb_temp)
+            forecast_data = get_forecast_data(city)
             print(f"City: {city}, {country}")
-            print(f"Temperature: {temp}°C")
-            print(f"Humidity: {humidity}%")
-            print(f"Wet Bulb Temperature: {wet_bulb_temp}°C")
-            print(f"Flag: {flag}")
+            print(f"Actual Temperature: {temp}°C")
+            print(f"Actual Humidity: {humidity}%")
+            print(f"Wet Bulb Temperature for now: {wet_bulb_temp}°C")
             print(alert_message)
+            future_date = datetime.datetime.now() + datetime.timedelta(days=5)
+            day_week_month = future_date.strftime("%A, %d %B")
+            print(f'Forecast {city}, {country}, for next 5 days {day_week_month}')
+            for forecast in forecast_data:
+                print(f"Date: {forecast['date']}")
+                print(f"Temperature: {forecast['temperature']}°C")
+                print(f"Humidity: {forecast['humidity']}%")
+                print(f"Condition: {forecast['condition']}")
         except Exception as e:
             print(e)
